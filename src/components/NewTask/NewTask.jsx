78a8a04662b8488db1efadb1dfb02c5bb966398c
Rafael Moret <rafaelmoretgalan@gmail.com>
2023-04-12 21:26:19 +0200

    add handle newtask with form, remove unnecessary state

--- src/components/NewTask/NewTask.jsx
+++ src/components/NewTask/NewTask.jsx
@@ -1,51 +1,51 @@
-import { useState } from "react"
 import "./NewTask.css"
 import "../Task/Task.css"
 import { AddIcon } from "../AddIcon/AddIcon"
 
 export function NewTask({ addTask }) {
-    const [title, setTitle] = useState("")
-    const [description, setDescription] = useState("")
+    const handleSubmit = (event) => {
+        event.preventDefault()
 
-    const handleTitle = title => setTitle(title.target.value)
-    const handleDescription = description => setDescription(description.target.value)
-    
-    const handleClick = () => {
-        if (title && description) {
+        const formData = new FormData(event.target)
+        const task = Object.fromEntries(formData.entries())
+
+        if (task.title && task.description) {
             addTask({ 
-                title: title, 
-                description: description, 
+                title: task.title, 
+                description: task.description, 
                 finished: false 
             })
-    
-            setTitle("")
-            setDescription("")
-        }
+
+            event.target.reset()
+        }    
     }
+
     const titlePlaceholder = "Enter a new task..."
     const descriptionPlaceholder = "Enter the description for the new task..."
 
     return (
         <article className="task-article">
-            <aside className="task-aside">
-                <button className="task-icon-button" onClick={handleClick}>
-                    <AddIcon />
-                </button>             
-            </aside>
-            <header className="task-header">
-                <input 
-                    className="newtask-input newtask-title task-title"
-                    placeholder={titlePlaceholder} 
-                    onChange={handleTitle}
-                    value={title}
-                />
-                <input 
-                    className="newtask-input newtask-description task-description"
-                    placeholder={descriptionPlaceholder} 
-                    onChange={handleDescription}
-                    value={description}
-                />                
-            </header>
+            <form onSubmit={handleSubmit} className="newtask-form">
+                <aside className="task-aside">
+                    <button className="task-icon-button" type="submit">
+                        <AddIcon />
+                    </button>             
+                </aside>
+                <header className="task-header">
+                    <input 
+                        className="newtask-input newtask-title task-title"
+                        placeholder={titlePlaceholder}
+                        defaultValue=""
+                        name="title"
+                    />
+                    <input 
+                        className="newtask-input newtask-description task-description"
+                        placeholder={descriptionPlaceholder} 
+                        defaultValue=""
+                        name="description"
+                    />              
+                </header>
+            </form>  
         </article>
     )
 }
